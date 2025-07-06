0425c1011ead83e85707cdedc69cabfccae8408b
sudhanshi133 <gargsudanshi@gmail.com>
2025-07-06 13:49:25 +0530

    first dumb commit

---
+++ src/main/java/com/example/project/pojo/journalPojo.java
@@ -0,0 +1,12 @@
+package com.example.project.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class journalPojo {
+    int id;
+    String title;
+    String content;
+}
