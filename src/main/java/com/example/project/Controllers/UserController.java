7c77ec8d65bc68ecdf1b032b4c6fb5ec48d57d2f
sudhanshi133 <gargsudanshi@gmail.com>
2025-07-13 14:07:05 +0530

    made a user

--- src/main/java/com/example/project/Controllers/UserController.java
+++ src/main/java/com/example/project/Controllers/UserController.java
@@ -1,4 +1,37 @@
 package com.example.project.Controllers;
 
+import com.example.project.Service.UserService;
+import com.example.project.pojo.UserPojo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+    @GetMapping
+    public List<UserPojo> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public void createUser(UserPojo user){
+        userService.saveUser(user);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateUser(UserPojo user){
+        UserPojo userInDb = userService.findByUsername(user.getUserName());
+        if(userInDb!=null) {
+            userInDb.setUserName(user.getUserName());
+            userInDb.setPassword(user.getPassword());
+            userService.saveUser(userInDb);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/example/project/Service/UserService.java
+++ src/main/java/com/example/project/Service/UserService.java
@@ -1,4 +1,33 @@
 package com.example.project.Service;
 
+import com.example.project.pojo.UserPojo;
+import com.example.project.repository.UserRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserPojo> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<UserPojo> getUserById(ObjectId id) {
+        return userRepository.findById(id);
+    }
+
+    public void saveUser(UserPojo user) {
+        userRepository.save(user);
+    }
+    public void deleteUser(ObjectId id) {
+        userRepository.deleteById(id);
+    }
+
+    public UserPojo findByUsername(String username){
+    	return userRepository.findByUsername(username);
+    }
 }
